b28d2dfd7469d2c32e50a3e9ed4b4b6fb92ccd8a
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-26 01:45:19 -0400

    table styling

--- src/Components/UserDirectory.js
+++ src/Components/UserDirectory.js
@@ -3,31 +3,26 @@ import "../style/UserDirectory.css";
 
 function UserDirectory(props) {
     return (
-        <table classname="userTable">
-        <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Phone</th>
-          <th>E-Mail</th>
-        </tr>
-  
-        <tbody className="tableBody">
-          {props.tableInputs.map((result) => (
-            <tr className="table" key={result.login.uuid}>
-              <td>
-                <img className="" src={result.picture.large} alt="" />
-              </td>
-  
-              <td>{`${result.name.first} ${result.name.last}`} </td>
-  
-              <td>{result.cell}</td>
-              <td className="email">
-                <a href={result.email}>{result.email}</a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div className="container"  id="userTable">
+            <table>
+                <tbody>
+                {props.tableInputs.map((result) => (
+                    <tr className="table" key={result.login.uuid}>
+                    <td>
+                        <img className="" src={result.picture.large} alt="" />
+                    </td>
+        
+                    <td class="align-middle">{`${result.name.first} ${result.name.last}`} </td>
+        
+                    <td class="align-middle">{result.cell}</td>
+                    <td class="align-middle">
+                        <a href={result.email}>{result.email}</a>
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
