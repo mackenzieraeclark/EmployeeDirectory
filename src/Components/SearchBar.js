ffb4f2eeaa247ba148817dd1efc8ad51532fc707
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-26 00:44:55 -0400

    function definition with html and export for each component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,10 @@
 import React from "react";
+import "../style/SearchBar.css";
+
+function SearchBar() {
+    return (
+    <div></div>
+    );
+};
+
+export default SearchBar;
--- src/Components/UserDirectory.js
+++ src/Components/UserDirectory.js
@@ -1,1 +1,10 @@
 import React from "react";
+import "../style/UserDirectory.css";
+
+function UserDirectory() {
+    return (
+    <div></div>
+    );
+};
+
+export default UserDirectory;
