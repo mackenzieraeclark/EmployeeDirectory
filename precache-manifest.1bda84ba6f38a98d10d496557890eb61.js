69f01c905ca34500522c0803284bdc2f99d60078
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-26 01:53:37 -0400

    Updates

---
+++ precache-manifest.1bda84ba6f38a98d10d496557890eb61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a4e6a32b14b2d365dc9d7890787d69",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c7ea21041bb70634e561",
+    "url": "/my-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3141c4e71d9b91678d06",
+    "url": "/my-app/static/css/main.90bc2dc3.chunk.css"
+  },
+  {
+    "revision": "c7ea21041bb70634e561",
+    "url": "/my-app/static/js/2.29c5714c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.29c5714c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3141c4e71d9b91678d06",
+    "url": "/my-app/static/js/main.cad2ac1c.chunk.js"
+  },
+  {
+    "revision": "59d605cb8d380f2be8a2",
+    "url": "/my-app/static/js/runtime-main.8b7c234a.js"
+  }
+]);
