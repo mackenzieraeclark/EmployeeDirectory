378f563f7e9d0c5a54eef7b82cee0c1f46a0071c
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-26 01:00:46 -0400

    header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,8 @@ import "../style/Header.css";
 function Header() {
     return (
     <header>
-        <h1 className="header">User Directory</h1>
-        <h3 className="header">Manage your user's information with the directory application. You can search for a user by name:</h3>
+        <h1 className="header1">User Directory</h1>
+        <h3 className="header2">Manage your user's information with the directory application. You can search for a user by name:</h3>
     </header>
     );
 };
