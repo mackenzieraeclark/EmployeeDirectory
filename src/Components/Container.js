2e93967c4ed082585dc9da061ef2b64ae4d0ad7b
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-09-25 23:27:15 -0400

    set up main container and export

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,1 +1,19 @@
+// Note for author: this page is the top (look at gallery example you drove for hierarchy)
+
 import React from "react";
+import React, {Component} from "react";
+
+// import from other pages to add into main
+import Header from "./Header";
+import SearchBar from "./SearchBar";
+import UserDirectory from "./UserDirectory";
+
+// create class
+class Container extends Component {
+
+    // here
+
+}
+
+// export class
+export default Container;
